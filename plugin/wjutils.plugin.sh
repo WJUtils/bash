914d53aefefcdef8e9ff644a197a850b33bfe32e
WillsterJohnson <willster@willsterjohnson.com>
2023-06-12 17:21:21 +0100

    color & style improvments

--- plugin/wjutils.plugin.sh
+++ plugin/wjutils.plugin.sh
@@ -1,24 +1,28 @@
 #! bash oh-my-bash.module
 
-WJUTILS="$OSH_CUSTOM/plugins/wjutils"
+INSTALL_DIR=$(echo "$OSH_CUSTOM" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+if [ -z "$INSTALL_DIR" ]; then
+  INSTALL_DIR="$OSH/custom"
+fi
+INSTALL_DIR="$INSTALL_DIR/plugins/wjutils"
 
-source "$WJUTILS/colors.sh"
-source "$WJUTILS/uninstall.sh"
+source "$INSTALL_DIR/colors.sh"
+source "$INSTALL_DIR/uninstall.sh"
 
 function wjutils_err() {
-  echo -e "$(red "[WJUtils]") $1"
+  echo -e "$(red "(✘)[WJUtils]") $1"
 }
 
 function wjutils_warn() {
-  echo -e "$(yellow "[WJUtils]") $1"
+  echo -e "$(yellow "(!)[WJUtils]") $1"
 }
 
 function wjutils_info() {
-  echo -e "$(cyan "[WJUtils]") $1"
+  echo -e "$(cyan "(i)[WJUtils]") $1"
 }
 
 function wjutils_success() {
-  echo -e "$(green "[WJUtils]") $1"
+  echo -e "$(green "(✔)[WJUtils]") $1"
 }
 
 function wjutils_helpinfo() {
@@ -41,15 +45,15 @@ function wjutilshelp() {
   echo ""
   echo "The recognised values are:"
   echo ""
-  echo "| Value      | Description                                                                |"
-  echo "| ---------- | -------------------------------------------------------------------------- |"
-  echo "| \`color\`    | Effects if command outputs (**not** aliases) will be colored.              |"
-  echo "| \`git\`      | Effects whether the Git aliases are enabled.                               |"
-  echo "| \`gitmoji\`  | Effects whether the Gitmoji aliases are enabled.                           |"
-  echo "| \`nodeload\` | Effect whether the Node Loader will load the Node version.                 |"
-  echo "| \`nvmload\`  | Effect whether the NVM Loader will update/install NVM.                     |"
-  echo "| \`pnpm\`     | Effects whether the PNPM aliases are enabled.                              |"
-  echo "| \`pnpmload\` | Effects whether the PNPM Loader will update/install PNPM.                  |"
+  echo "$(cyan "│") Value    $(cyan "│") Description                                                   $(cyan "│")"
+  echo "$(cyan "├──────────┼───────────────────────────────────────────────────────────────┤")"
+  echo "$(cyan "│") $(magenta "color")    $(cyan "│") Effects if command outputs (**not** aliases) will be colored. $(cyan "│")"
+  echo "$(cyan "│") $(magenta "git")      $(cyan "│") Effects whether the Git aliases are enabled.                  $(cyan "│")"
+  echo "$(cyan "│") $(magenta "gitmoji")  $(cyan "│") Effects whether the Gitmoji aliases are enabled.              $(cyan "│")"
+  echo "$(cyan "│") $(magenta "nodeload") $(cyan "│") Effect whether the Node Loader will load the Node version.    $(cyan "│")"
+  echo "$(cyan "│") $(magenta "nvmload")  $(cyan "│") Effect whether the NVM Loader will update/install NVM.        $(cyan "│")"
+  echo "$(cyan "│") $(magenta "pnpm")     $(cyan "│") Effects whether the PNPM aliases are enabled.                 $(cyan "│")"
+  echo "$(cyan "│") $(magenta "pnpmload") $(cyan "│") Effects whether the PNPM Loader will update/install PNPM.     $(cyan "│")"
 }
 
 # why be smart when a dumb thing works?
@@ -62,25 +66,26 @@ else
 fi
 
 if [[ ! $WJU_DISABLE =~ " nvmload " ]]; then
-  source "$WJUTILS/nvmloader.sh"
+  source "$INSTALL_DIR/nvmloader.sh"
 fi
 
 if [[ ! $WJU_DISABLE =~ " nodeload " ]]; then
-  source "$WJUTILS/nodeloader.sh"
+  source "$INSTALL_DIR/nodeloader.sh"
 fi
 
 if [[ ! $WJU_DISABLE =~ " pnpmload " ]]; then
-  source "$WJUTILS/pnpmloader.sh"
+  source "$INSTALL_DIR/pnpmloader.sh"
 fi
 if [[ ! $WJU_DISABLE =~ " pnpm " ]]; then
-  source "$WJUTILS/pnpmaliases.sh"
+  source "$INSTALL_DIR/pnpmaliases.sh"
 fi
 
 if [[ ! $WJU_DISABLE =~ " git " ]]; then
-  source "$WJUTILS/gitaliases.sh"
+  source "$INSTALL_DIR/gitaliases.sh"
 fi
 if [[ ! $WJU_DISABLE =~ " gitmoji " ]]; then
-  source "$WJUTILS/gitmojialiases.sh"
+  source "$INSTALL_DIR/gitmojialiases.sh"
 fi
 
 wjutils_info "Until an auto-update script is added, check https://github.com/WJUtils/bash for updates."
+wjutilshelp
