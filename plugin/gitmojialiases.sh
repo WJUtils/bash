099d10703931747f7cf3afb28cde1a733818cac3
WillsterJohnson <willster@willsterjohnson.com>
2023-06-12 18:14:05 +0100

    color & style improvments

--- plugin/gitmojialiases.sh
+++ plugin/gitmojialiases.sh
@@ -3,8 +3,8 @@ alias gtmjc="gitmoji -c"
 
 function gtmjhelp() {
   echo -e "Git Shorthands"
-  echo -e "  $(blue "gtmj")  $(grey "|") gitmoji       $(grey "|") \`$(green "gitmoji")\`                             "
-  echo -e "  $(blue "gtmjc") $(grey "|") gitmoji -c    $(grey "|") \`$(green "gitmoji -c")\`                          "
+  echo -e "  $(blue "gtmj")  │ gitmoji       │ \`$(green "gitmoji")\`                             "
+  echo -e "  $(blue "gtmjc") │ gitmoji -c    │ \`$(green "gitmoji -c")\`                          "
 }
 
 wjutils_helpinfo "Gitmoji Shorthands" "gtmjhelp"
