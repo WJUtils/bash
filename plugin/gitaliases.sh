89034e19661d15e94ec6084521f521d0db63014b
WillsterJohnson <willster@willsterjohnson.com>
2023-06-13 12:21:29 +0100

    add completions to git

--- plugin/gitaliases.sh
+++ plugin/gitaliases.sh
@@ -3,16 +3,27 @@
 alias gt="git"
 function gtcn() { git clone "git@github.com:$1" "$2"; }
 alias gta="git add"
+__git_complete gta _git_add
 alias gtc="git commit"
+__git_complete gtc _git_commit
 alias gtcm="git commit -m"
+__git_complete gtcm _git_commit
 alias gtpl="git pull"
+__git_complete gtpl _git_pull
 alias gtps="git push"
+__git_complete gtps _git_push
 alias gtsh="git stash"
+__git_complete gtsh _git_stash
 alias gtr="git remote"
+__git_complete gtr _git_remote
 alias gts="git status"
+__git_complete gts _git_status
 alias gtl="git log"
+__git_complete gtl _git_log
 alias gtb="git branch"
+__git_complete gtb _git_branch
 alias gtch="git checkout"
+__git_complete gtch _git_checkout
 
 function gthelp() {
   echo -e "Git Shorthands"
