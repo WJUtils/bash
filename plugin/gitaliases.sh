10cb1a7b0f4a9cc6d23d8aac884f24dcbf3468a2
WillsterJohnson <willster@willsterjohnson.com>
2023-06-19 18:50:52 +0100

    auto source git completion

--- plugin/gitaliases.sh
+++ plugin/gitaliases.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 
+__WJUITLS_GITALIASES_DO_GIT_COMPLETE=1
+if [ -f /etc/bash_completion.d/git ]; then
+  source /etc/bash_completion.d/git
+elif [ -f /usr/share/bash-completion/completions/git ]; then
+  source /usr/share/bash-completion/completions/git
+else
+  __WJUITLS_GITALIASES_DO_GIT_COMPLETE=0
+fi
+
 alias gt="git"
+[ "$__WJUITLS_GITALIASES_DO_GIT_COMPLETE" = "1" ] && __git_complete gt _git
 function gtcn() { git clone "git@github.com:$1" "$2"; }
 alias gta="git add"
-__git_complete gta _git_add
+[ "$__WJUITLS_GITALIASES_DO_GIT_COMPLETE" = "1" ] && __git_complete gta _git_add
 alias gtc="git commit"
-__git_complete gtc _git_commit
+[ "$__WJUITLS_GITALIASES_DO_GIT_COMPLETE" = "1" ] && __git_complete gtc _git_commit
 alias gtcm="git commit -m"
-__git_complete gtcm _git_commit
+[ "$__WJUITLS_GITALIASES_DO_GIT_COMPLETE" = "1" ] && __git_complete gtcm _git_commit
 alias gtpl="git pull"
-__git_complete gtpl _git_pull
+[ "$__WJUITLS_GITALIASES_DO_GIT_COMPLETE" = "1" ] && __git_complete gtpl _git_pull
 alias gtps="git push"
-__git_complete gtps _git_push
+[ "$__WJUITLS_GITALIASES_DO_GIT_COMPLETE" = "1" ] && __git_complete gtps _git_push
 alias gtsh="git stash"
-__git_complete gtsh _git_stash
+[ "$__WJUITLS_GITALIASES_DO_GIT_COMPLETE" = "1" ] && __git_complete gtsh _git_stash
 alias gtr="git remote"
-__git_complete gtr _git_remote
+[ "$__WJUITLS_GITALIASES_DO_GIT_COMPLETE" = "1" ] && __git_complete gtr _git_remote
 alias gts="git status"
-__git_complete gts _git_status
+[ "$__WJUITLS_GITALIASES_DO_GIT_COMPLETE" = "1" ] && __git_complete gts _git_status
 alias gtl="git log"
-__git_complete gtl _git_log
+[ "$__WJUITLS_GITALIASES_DO_GIT_COMPLETE" = "1" ] && __git_complete gtl _git_log
 alias gtb="git branch"
-__git_complete gtb _git_branch
+[ "$__WJUITLS_GITALIASES_DO_GIT_COMPLETE" = "1" ] && __git_complete gtb _git_branch
 alias gtch="git checkout"
-__git_complete gtch _git_checkout
+[ "$__WJUITLS_GITALIASES_DO_GIT_COMPLETE" = "1" ] && __git_complete gtch _git_checkout
 
 function gthelp() {
   echo -e "Git Shorthands"
