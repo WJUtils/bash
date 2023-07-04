3a77336494551ee25cc5c0b0917a86493b0521f7
WillsterJohnson <willster@willsterjohnson.com>
2023-07-04 12:40:17 +0100

    fix node/pnpm loader quirks

--- plugin/nodeloader.sh
+++ plugin/nodeloader.sh
@@ -1,38 +1,34 @@
+function __nvmu_lts() {
+  nvm install --lts >&/dev/null
+  nvm use --lts >&/dev/null
+}
+
 # $1 `1` for silent, `0` for verbose. default is `0`
 function nvmu() {
   # print "test"
   [ "$1" != "1" ] && __wjutils_info "Ensuring Node version..."
-  {
-    nvm install >&/dev/null
-    nvm use >&/dev/null
-  } || {
-    if [ ! -f "./package.json" ]; then
-      $(exit 1)
-    elif [ "$(cat "./package.json" | grep -E "^[[:space:]]*\"engines\"[[:space:]]*:[[:space:]]*{" | grep -E "^[[:space:]]*\"node\"[[:space:]]*:" | wc -l)" = "0" ]; then
-      $(exit 1)
-    elif [ "$(cat "./package.json" | grep -E "^[[:space:]]*\"node\"[[:space:]]*:" | wc -l)" = "0" ]; then
-      $(exit 1)
-    fi
-    if ! command -v node &>/dev/null; then
-      __wjutils_err "Node is not installed but a \`package.json\` file was found with a potential Node engine version."
-      __wjutils_info "Installing LTS to parse JSON..."
-      nvm install --lts >&/dev/null
-      nvm use --lts >&/dev/null
-    fi
+
+  HAS_NVMRC=$(cat ./.nvmrc 2>/dev/null)
+  HAS_PACKAGEJSON=$(cat ./package.json 2>/dev/null)
+
+  if [ "$HAS_NVMRC" != "" ]; then
+    nvm install "$version" >&/dev/null
+  elif [ "$HAS_PACKAGEJSON" != "" ]; then
     local version="$(__wjutils_json_read "localpackagejson" "engines.node")"
     if [[ "$version" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]] || [[ "$version" =~ ^~[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
       version="$(echo "$version" | sed "s/^~//g")"
     elif [[ "$version" =~ ^.*\<[0-9]+$ ]]; then
       version="$(echo "$version" | sed "s/^.*\<//g" | awk '{print $1-1}')"
     else
-      $(exit 1)
+      nvm install --lts >&/dev/null
     fi
-    nvm install "$version" >&/dev/null
-    nvm use "$version" >&/dev/null
-  } || {
+    if [[ "$version" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+      nvm install "$version" >&/dev/null
+    fi
+  else
     nvm install --lts >&/dev/null
-    nvm use --lts >&/dev/null
-  }
+  fi
+
   [ "$1" != "1" ] && __wjutils_success "Now using: node $(green "$(node --version)").\tTo re-ensure Node version, run \`$(blue "nvmu")\` again."
 }
 
--- plugin/pnpmloader.sh
+++ plugin/pnpmloader.sh
@@ -1,7 +1,6 @@
 function ppcp() {
   __wjutils_info "Ensuring PNPM version..."
-  corepack enable >&/dev/null
-  corepack prepare pnpm@latest --activate >&/dev/null
+  npm i -g pnpm >&/dev/null
   __wjutils_success "Now using: pnpm $(green "$(pnpm --version)").\tTo re-ensure PNPM version, run \`$(blue "ppcp")\` again."
 }
 
